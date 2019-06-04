93ac6028d12e9afdfc6655e705dbd05130723e04
Marilia <marilia.mfernandez@gmail.com>
2019-06-04 16:52:28 -0300

    Add tabela clientes e filmes

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var http = require('http'),
     express = require('express'),
     path = require('path'),
-    app = express();
+    app = express(),
+    Users = require('./model/Clientes'),
+    Filmes = require('./model/Filmes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'img')));
@@ -12,6 +14,16 @@ app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+app.post('/cadastrar_usuario', (req,res) => {
+
+    Users.insert(req.body.usuario, req.body.senha).then(() => {
+        console.log('Bem vindo ' + Users.find(req.body.usuario))
+    })
+    
+
+    res.end();
 });
 
 http.createServer(app).listen(3000);
